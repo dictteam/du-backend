932c5e0a710e7c16c1b9bda95e2b5d039b5b30fa
4strodev <jumarurex@gmail.com>
2024-07-18 21:06:28 +0200

    feat:create basic app

--- internal/cmd/main/main.go
+++ internal/cmd/main/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/recover"
+)
 
 func main() {
-	fmt.Println("This is a project template! remember change package name")
+	app := fiber.New()
+
+	app.Use(recover.New())
+	app.Use(logger.New())
+
+	app.Get("/", func(ctx *fiber.Ctx) error {
+		return ctx.JSON(fiber.Map{
+			"msg": "Hello, World!",
+		})
+	})
+
+	app.Get("/ping", func(ctx *fiber.Ctx) error {
+		return ctx.SendString("pong!")
+	})
+
+	log.Fatal(app.Listen(":3000"))
 }
